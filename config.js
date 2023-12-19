ecb22cdfa3ef7a6ce7b4b1ba6f60a6bed57d0fb6
SammyTechies <151462311+SammyTechies@users.noreply.github.com>
2023-12-19 02:57:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['2349155610431', 'sammix', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
